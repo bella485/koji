233149cb88b605caab5cc8397b425937a7369b6a
Mike McLean <mikem@redhat.com>
2018-10-03 15:26:19 -0400

    Split out inheritance flags formatting

--- cli/koji_cli/commands.py
+++ cli/koji_cli/commands.py
@@ -40,7 +40,7 @@ from koji_cli.lib import _, OptionParser, activate_session, parse_arches, \
         _unique_path, _running_in_bg, _progress_callback, watch_tasks, \
         arg_filter, linked_upload, list_task_output_all_volumes, \
         print_task_headers, print_task_recurse, download_file, watch_logs, \
-        error, greetings, _list_tasks, unique_path
+        error, greetings, _list_tasks, unique_path, format_inheritance_flags
 
 
 def _printable_unicode(s):
@@ -4670,17 +4670,7 @@ def anon_handle_taginfo(goptions, session, args):
                 print("  %(priority)3i %(external_repo_name)s (%(url)s)" % rinfo)
         print("Inheritance:")
         for parent in session.getInheritanceData(tag, **event_opts):
-            flags = ''
-            for code,expr in (
-                    ('M',parent['maxdepth'] is not None),
-                    ('F',parent['pkg_filter']),
-                    ('I',parent['intransitive']),
-                    ('N',parent['noconfig']),):
-                if expr:
-                    flags += code
-                else:
-                    flags += '.'
-            parent['flags'] = flags
+            parent['flags'] = format_inheritance_flags(parent)
             print("  %(priority)-4d %(flags)s %(name)s [%(parent_id)s]" % parent)
             if parent['maxdepth'] is not None:
                 print("    maxdepth: %(maxdepth)s" % parent)
