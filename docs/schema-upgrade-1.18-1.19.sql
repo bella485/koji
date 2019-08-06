fef33eb313c2019acce24b3e1eb8d5d0d05887f2
Ben Howard <ben.howard@redhat.com>
2019-08-06 17:12:14 -0600

    docs/schema-upgrade-1.18-1.19.sql: additional CoreOS artifact types.

--- docs/schema-upgrade-1.18-1.19.sql
+++ docs/schema-upgrade-1.18-1.19.sql
@@ -4,5 +4,13 @@
 
 BEGIN;
 
+-- add compressed iso-compressed, vhd-compressed, vhdx-compressed, and vmdk-compressed
+insert into archivetypes (name, description, extensions) values ('iso-compressed', 'Compressed iso image', 'iso.gz iso.xz');
+insert into archivetypes (name, description, extensions) values ('vhd-compressed', 'Compressed VHD image', 'vhd.gz vhd.xz');
+insert into archivetypes (name, description, extensions) values ('vhdx-compressed', 'Compressed VHDx image', 'vhd.gz vhd.xz');
+insert into archivetypes (name, description, extensions) values ('vmdk-compressed', 'Compressed VMDK image', 'vmdk.gz vmdk.xz');
 
+-- add kernel-image and imitramfs
+insert into archivetypes (name, description, extensions) values ('kernel-image', 'Kernel BZ2 Image', 'vmlinuz vmlinuz.gz vmlinuz.xz');
+insert into archivetypes (name, description, extensions) values ('initramfs', 'Compressed Initramfs Image', 'img');
 COMMIT;
