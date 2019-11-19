79c3f4e44f77eb580af9b9393864a4503d1e2597
Tomas Kopecek <tkopecek@redhat.com>
2019-11-19 13:53:44 +0100

    browsable api
    Fixes: https://pagure.io/koji/issue/1813

--- cli/koji_cli/commands.py
+++ cli/koji_cli/commands.py
@@ -2445,10 +2445,7 @@ def anon_handle_list_api(goptions, session, args):
         parser.error(_("This command takes no arguments"))
         assert False  # pragma: no cover
     activate_session(session, goptions)
-    tmplist = [(x['name'], x) for x in session._listapi()]
-    tmplist.sort()
-    funcs = [x[1] for x in tmplist]
-    for x in funcs:
+    for x in sorted(session._listapi(), key=lambda x: x['name']):
         if 'argdesc' in x:
             args = x['argdesc']
         elif x['args']:
