7fe80a1e9ce40b58ee8f36b079d490945802c757
Tomas Kopecek <tkopecek@redhat.com>
2021-09-20 11:42:02 +0200

    hub: deprecate taskReport
    Fixes: https://pagure.io/koji/issue/3031

--- hub/kojihub.py
+++ hub/kojihub.py
@@ -12691,6 +12691,7 @@ class RootExports(object):
 
     def taskReport(self, owner=None):
         """Return data on active or recent tasks"""
+        logger.warning("taskReport call is deprecated and will be removed in 1.29")
         fields = (
             ('task.id', 'id'),
             ('task.state', 'state'),
