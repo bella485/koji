bella485/koji

commit 355c713044a7fd81aa867281b65b20cf3804bd8d

Taginfo page shows packages with/without blocked
Fixes: https://pagure.io/koji/issue/1350

hub/kojihub.py
@@ -1121,7 +1121,7 @@ def pkglist_setarches(taginfo, pkginfo, arches, force=False):
 
 
 def readPackageList(tagID=None, userID=None, pkgID=None, event=None, inherit=False,
-                    with_dups=False, with_owners=True):
+                    with_dups=False, with_owners=True, with_blocked=True):
     """Returns the package list for the specified tag or user.
 
     One of (tagID,userID,pkgID) must be specified
@@ -1175,6 +1175,8 @@ def readPackageList(tagID=None, userID=None, pkgID=None, event=None, inherit=Fal
             'users ON users.id = tag_package_owners.owner'
         ]
         clauses.append(eventCondition(event, table='tag_package_owners'))
+    if not with_blocked:
+        clauses.append('NOT tag_packages.blocked')
     fields, aliases = zip(*fields)
     query = QueryProcessor(columns=fields, aliases=aliases, tables=tables, joins=joins,
                            clauses=clauses, values=locals())
@@ -12285,7 +12287,8 @@ class RootExports(object):
     getPackage = staticmethod(lookup_package)
 
     def listPackages(self, tagID=None, userID=None, pkgID=None, prefix=None, inherited=False,
-                     with_dups=False, event=None, queryOpts=None, with_owners=True):
+                     with_dups=False, event=None, queryOpts=None, with_owners=True,
+                     with_blocked=False):
         """
         Returns a list of packages in Koji.
 
@@ -12330,7 +12333,8 @@ class RootExports(object):
                 pkgID = get_package_id(pkgID, strict=True)
             result_list = list(readPackageList(tagID=tagID, userID=userID, pkgID=pkgID,
                                                inherit=inherited, with_dups=with_dups,
-                                               event=event, with_owners=with_owners).values())
+                                               event=event, with_owners=with_owners,
+                                               with_blocked=with_blocked).values())
             if with_dups:
                 # when with_dups=True, readPackageList returns a list of list of dicts
                 # convert it to a list of dicts for consistency

cli/koji_cli/commands.py
@@ -3210,8 +3210,15 @@ def anon_handle_list_pkgs(goptions, session, args):
         if options.show_blocked:
             parser.error("--show-blocked makes sense only with --tag,"
                          " --owner or --package")
+    if options.show_blocked:
+        opts['with_blocked'] = options.show_blocked
 
-    data = session.listPackages(**opts)
+    try:
+        data = session.listPackages(**opts)
+    except koji.ParameterError:
+        del opts['with_blocked']
+        data = session.listPackages(**opts)
+
     if not data:
         error("(no matching packages)")
     if not options.quiet:

www/kojiweb/index.py
@@ -984,8 +984,11 @@ def taginfo(environ, tagID, all='0', packageOrder='package_name', packageStart=N
     all = int(all)
 
     numPackages = server.count('listPackages', tagID=tag['id'], inherited=True, with_owners=False)
+    numPackagesBlocked = server.count('listPackages', tagID=tag['id'], inherited=True,
+                                      with_owners=False, with_blocked=True)
     numBuilds = server.count('listTagged', tag=tag['id'], inherit=True)
     values['numPackages'] = numPackages
+    values['numPackagesBlocked'] = numPackagesBlocked
     values['numBuilds'] = numBuilds
 
     inheritance = server.getFullInheritance(tag['id'])

www/kojiweb/taginfo.chtml
@@ -124,6 +124,10 @@
       <td><a href="packages?tagID=$tag.id">$numPackages</a></td>
     </tr>
     <tr>
+      <th>Packages (blocked packages included)</th>
+      <td><a href="packages?tagID=$tag.id">$numPackagesBlocked</a></td>
+    </tr>
+    <tr>
       <th>Builds</th>
       <td><a href="builds?tagID=$tag.id">$numBuilds</a></td>
     </tr>

commit 8094fcadb949fda487f82991fb968d80e3effffb

hub: default with_blocked=True in listPackages
Commit 355c713044a7fd81aa867281b65b20cf3804bd8d introduced a regression
in the listPackages RPC, where it no longer returns blocked packages by
default.

To restore backwards compatibility, set the with_blocked argument to
True by default.

hub/kojihub.py
@@ -12305,7 +12305,7 @@ class RootExports(object):
 
     def listPackages(self, tagID=None, userID=None, pkgID=None, prefix=None, inherited=False,
                      with_dups=False, event=None, queryOpts=None, with_owners=True,
-                     with_blocked=False):
+                     with_blocked=True):
         """
         Returns a list of packages in Koji.
 